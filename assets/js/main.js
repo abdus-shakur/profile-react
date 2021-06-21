0a7065acbe4fca29887e38c4d6c7a14d724bf505
Shakur <abdusshakurt@gmail.com>
2021-06-22 00:31:30 +0530

    date tag

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,6 +99,7 @@ var experience = now.diff(experienceStart, 'years', true);
   $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
   console.log(JSON.stringify(data, null, 2));
   localStorage.setItem("dt_obj",JSON.stringify(data, null, 2));
+  localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
   localStorage.setItem("dt",data.ipAddress);
 });
 
