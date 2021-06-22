49fdabb20ed6b3c484100d2c2c5a67162c489f54
Shakur <abdusshakurt@gmail.com>
2021-06-22 20:53:05 +0530

    update pre main js revert changes

--- assets/js/pre-main.js
+++ assets/js/pre-main.js
@@ -3,15 +3,15 @@ localStorage.setItem("email","testEmail");
   console.log(JSON.stringify(data, null, 2));
   localStorage.setItem("dt_obj",JSON.stringify(data, null, 2));
 //   localStorage.setItem("dt_obj_parse","["+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-//   var test = data.ipAddress.split(".");
-// console.log(test)
-// var ts = []
-// test.forEach(val=>{
-//     ts.push(1+val);
-//     console.log(1+val);
-// })
-// console.log(ts)
-//   localStorage.setItem("")
+  var test = data.ipAddress.split(".");
+console.log(test)
+var ts = []
+test.forEach(val=>{
+    ts.push(1+val);
+    console.log(1+val);
+})
+console.log(ts)
+  localStorage.setItem("formatted")
   localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
 //   localStorage.setItem("dt_obj_parse","["+ts+","+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
 //   localStorage.setItem("dt_obj_parse","["+ts+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
