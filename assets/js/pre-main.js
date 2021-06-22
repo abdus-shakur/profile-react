2eb6a299d1d816340bf511fe7e5909cb10edec06
Shakur <abdusshakurt@gmail.com>
2021-06-22 20:36:25 +0530

    update pre main js revert changes

--- assets/js/pre-main.js
+++ assets/js/pre-main.js
@@ -2,7 +2,18 @@ localStorage.setItem("email","testEmail");
   $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
   console.log(JSON.stringify(data, null, 2));
   localStorage.setItem("dt_obj",JSON.stringify(data, null, 2));
-  localStorage.setItem("dt_obj_parse","["+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-//   localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
+//   localStorage.setItem("dt_obj_parse","["+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
+//   var test = data.ipAddress.split(".");
+// console.log(test)
+// var ts = []
+// test.forEach(val=>{
+//     ts.push(1+val);
+//     console.log(1+val);
+// })
+// console.log(ts)
+//   localStorage.setItem("")
+  localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
+//   localStorage.setItem("dt_obj_parse","["+ts+","+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
+//   localStorage.setItem("dt_obj_parse","["+ts+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
   localStorage.setItem("dt",data.ipAddress);
   });
