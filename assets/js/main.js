3ff92e9d8a1321ec7c24960de9540058b6e87313
Shakur <abdusshakurt@gmail.com>
2021-06-22 19:03:13 +0530

    update pre main js revert changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,13 +95,7 @@ var experience = now.diff(experienceStart, 'years', true);
     })
   }
 
-  localStorage.setItem("email","testEmail");
-  $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-  localStorage.setItem("dt_obj",JSON.stringify(data, null, 2));
-  localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-  localStorage.setItem("dt",data.ipAddress);
-});
+  
 
   /**
    * Header fixed top on scroll
