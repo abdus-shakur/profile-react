ea43503aaab0583f610dd901c1486740ddc8f5cc
Shakur <abdusshakurt@gmail.com>
2021-06-23 17:40:11 +0530

    add html changes

--- assets/js/pre-main.js
+++ assets/js/pre-main.js
@@ -1,19 +1,16 @@
-localStorage.setItem("email","testEmail");
-  $.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
-  console.log(JSON.stringify(data, null, 2));
-  localStorage.setItem("dt_obj",JSON.stringify(data, null, 2));
-//   localStorage.setItem("dt_obj_parse","["+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
+const userAction = async () => {
+  await (await fetch('https://integrated-services.herokuapp.com/data-service/save')).json();
+}
+userAction();
+localStorage.setItem("email", "testEmail");
+$.getJSON('https://api.db-ip.com/v2/free/self', function (data) {
+  localStorage.setItem("dt_obj", JSON.stringify(data, null, 2));
   var test = data.ipAddress.split(".");
-console.log(test)
-var ts = []
-test.forEach(val=>{
-    ts.push(1+val);
-    console.log(1+val);
-})
-console.log(ts)
-  localStorage.setItem("formatted",ts);
-  localStorage.setItem("dt_obj_parse","["+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-//   localStorage.setItem("dt_obj_parse","["+ts+","+data.ipAddress+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-//   localStorage.setItem("dt_obj_parse","["+ts+","+data.city+","+data.stateProv+","+data.countryName+","+data.countryCode+","+data.continentCode+","+data.continentName+"]");
-  localStorage.setItem("dt",data.ipAddress);
-  });
+  var ts = []
+  test.forEach(val => {
+    ts.push(1 + val);
+  })
+  localStorage.setItem("formatted", ts);
+  localStorage.setItem("dt_obj_parse", "[" + data.ipAddress + "," + data.city + "," + data.stateProv + "," + data.countryName + "," + data.countryCode + "," + data.continentCode + "," + data.continentName + "]");
+  localStorage.setItem("dt", data.ipAddress);
+});
