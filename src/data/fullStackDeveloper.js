3e506e857fcab15b48dd2403eb0884442e1b5ab6
Shakur <abdusshakurt@gmail.com>
2023-03-27 11:41:38 +0530

    Git Pages Build from NPM

---
+++ src/data/fullStackDeveloper.js
@@ -0,0 +1,376 @@
+var profileData = {
+    "id": "63cd5a30dfe91e5d2080d334",
+    "name": "Shakur",
+    "indicationType": "Java Full Stack Developer",
+    "experience": "2023-01-22T15:48:14.118+00:00",
+    "experienceValue": null,
+    "designation": "Java Full-Stack Developer",
+    "resumeUrl": "https://drive.google.com/file/d/1lRA3v7t6JlQQSTjtrAx6zjMwVP5-vrKh/view?usp=share_link",
+    "about": {
+        "description": [
+            "I was Born and brought up in <strong><em>Tamilnadu, India</em></strong> . Growing up, I had a unique perspective of the world - being indian and being an Engineer. I really enjoy learning new things and am constantly seeking out new learning opportunities. I'm not afraid of failure. In fact, I think it is an essential part of the experimental process that gets you to success.<br /><br /> I graduated from the Sethu Institute of technology, Madurai, Tamil Nadu, with a B.E in Mechanical Engineering as an Institute level <strong><em>Bronze Medalist</em></strong>."
+        ],
+        "aboutObjects": [
+            {
+                "icon": "bi bi-clock",
+                "value": "3",
+                "decimals": "2",
+                "description": "<strong>Years of experience</strong> in the field of Information Technology and services, providing Backend API and UI development services with varied technologies based upon the requirements."
+            },
+            {
+                "icon": "bi bi-emoji-smile",
+                "value": "15",
+                "decimals": "0",
+                "description": "<strong>Projects</strong> Completed up to date including professional and own creational projects."
+            },
+            {
+                "icon": "bi bi-journal-richtext",
+                "value": "8",
+                "decimals": "0",
+                "description": "<strong>Certifications </strong>done with readily available online validation of the certificates from Leading E-Learning Providers."
+            },
+            {
+                "icon": "bi bi-clock",
+                "value": "14",
+                "decimals": "0",
+                "description": "<strong>Proficient Technologies</strong> with professional handson experience in End to End Testing, Automation, Backend-Web Development and Devops."
+            },
+            {
+                "icon": "bi bi-award",
+                "value": "9",
+                "decimals": "0",
+                "description": "<strong>Awards and Recognition</strong> from Multi-national company for professional outstanding performance, team work, out-of-box thinking with recognized certificates from the organization"
+            }
+        ]
+    },
+    "skill": {
+        "description": "I'am an Excellent problem solver with good analytical skills, solid understanding of a software development and computer programming, highly organized and creative, possess engineering leadership, strong research skills, excellent written and communication skills with high proficiency in writing automation scripts.",
+        "skills": [
+            {
+                "name": "Java",
+                "value": "95"
+            },
+            {
+                "name": "Rest-Assured",
+                "value": "90"
+            },
+            {
+                "name": "Appium",
+                "value": "85"
+            },
+            {
+                "name": "Postman",
+                "value": "100"
+            },
+            {
+                "name": "Jenkins",
+                "value": "70"
+            },
+            {
+                "name": "React",
+                "value": "85"
+            },
+            {
+                "name": "HTML",
+                "value": "70"
+            },
+            {
+                "name": "Selenium",
+                "value": "100"
+            },
+            {
+                "name": "JMeter",
+                "value": "100"
+            },
+            {
+                "name": "GIT",
+                "value": "90"
+            },
+            {
+                "name": "Spring Boot",
+                "value": "75"
+            },
+            {
+                "name": "Maven",
+                "value": "80"
+            }
+        ]
+    },
+    "certification": [
+        {
+            "icon": "bx bx-cog",
+            "title": "REST API Automation",
+            "url": "https://www.udemy.com/certificate/UC-9710310d-4154-4c88-9358-a68cdab513a6/",
+            "description": "Rest API Backend web services automation, end to end automation along with best practices and real time application testing"
+        },
+        {
+            "icon": "bx bx-cog",
+            "title": "Selenium WebDriver with Java - Advanced + Frameworks",
+            "url": "https://www.udemy.com/certificate/UC-4734a1b3-f9cd-4d4c-a782-a4a828852c11/",
+            "description": "Selenium Automation of Any Real world web Applications, along with custom framework building from scratch"
+        },
+        {
+            "icon": "bx bx-tachometer",
+            "title": "JMeter - Performance Testing",
+            "url": "https://www.udemy.com/certificate/UC-e37c69f0-efdc-4683-841c-e6513c1de00e/",
+            "description": "Design Robust Performance Testcases in Jmeter tool with the help of available features and Monitor the Performance of application with different Load Parameters"
+        },
+        {
+            "icon": "bx bx-globe",
+            "title": "Java Web Development",
+            "url": "https://www.udemy.com/certificate/UC-7538b86e-9ea8-4a54-8c3b-700a899fb986/",
+            "description": "Indepth Understanding of Building Backend for Web Applications from Scratch"
+        },
+        {
+            "icon": "bx bx-git-repo-forked",
+            "title": "Git Complete - The Definitive Guide",
+            "url": "https://www.udemy.com/certificate/UC-b9c9a89d-460d-41d3-bac1-a6bf4a5b75dc/",
+            "description": "Very Solid Understanding and Hands-On Experience with Git and related source control concepts."
+        },
+        {
+            "icon": "bx bxl-figma",
+            "title": "Figma UI / UX Design Essentials",
+            "url": "https://www.udemy.com/certificate/UC-fcad9e51-ca1a-43ee-bfa7-2d1fd54b6018/",
+            "description": "Design Interface using Powerfull tools like constraints for responsive layouts"
+        },
+        {
+            "icon": "bx bx-abacus",
+            "title": "GMath - Permutations and Combinations",
+            "url": "https://www.udemy.com/certificate/UC-e282a8d7-1557-4960-9b48-18de348158a9/",
+            "description": "Well Researched Explanations as well as time saving techniques for Solving Permutations and Combinations (PnC) problems"
+        }
+    ],
+    "contact": {
+        "description": "Reach me out any time @ social media sites",
+        "location": {
+            "name": "Madurai, Tamilnadu, India",
+            "url": "https://www.google.co.in/maps/place/Madurai,+Tamil+Nadu/"
+        },
+        "email": "abdusshakurt@gmail.com",
+        "twitter": "https://twitter.com/abdusshakur1412",
+        "facebook": "https://www.facebook.com/smart.shakur/#",
+        "linkedIn": "https://www.linkedin.com/in/abdus-shakur/",
+        "messageReference": ""
+    },
+    "awards": {
+        "description": "",
+        "details": [
+            {
+                "description": null,
+                "category": "Awards",
+                "align": "LEFT",
+                "categoryDetails": [
+                    {
+                        "title": "On the Spot Awards",
+                        "highlight": "3 Awards",
+                        "summary": "<strong><em>Tata Consultancy Services, Kochi, Kerala, India</em></strong>",
+                        "descriptionType": null,
+                        "description": [
+                            "For Ensuring the Quality of organization delieverables, with Increased attention to details and impeccable knowledge of the domain."
+                        ],
+                        "shortDescription": [
+                            "Created Highest number of automation scripts in the team at the critical delieverable period.",
+                            "Provided the leadership with extra edge to talk about the wealth of knowledge regarding domain or technology to the customers."
+                        ]
+                    },
+                    {
+                        "title": "Team Awards",
+                        "highlight": "3 Awards",
+                        "summary": "<strong><em>Tata Consultancy Services, Kochi, Kerala, India</em></strong>",
+                        "descriptionType": null,
+                        "description": [
+                            "Best team award based on the appreciations from client and inclusive of 2 On-the-spot award for exceptional performance as a team."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Insitute Level Medalist",
+                        "highlight": "Bachelor of Engineering in Mechanical",
+                        "summary": "<strong><em>Completed Bachelors degree with distinction & Institute level bronze medalist</em></strong>",
+                        "descriptionType": null,
+                        "description": null,
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Applause Award",
+                        "highlight": "Out-Of-The-Box thinking",
+                        "summary": "<strong><em>Tata Consultancy Services, Kochi, Kerala, India</em></strong>",
+                        "descriptionType": null,
+                        "description": [
+                            "For solving critical customer issue with a simple java program based tool."
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            },
+            {
+                "description": null,
+                "category": "Java Web Services",
+                "align": "LEFT",
+                "categoryDetails": [
+                    {
+                        "title": "Virtualized Data Service",
+                        "highlight": "Spring Boot",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Virtual data service which records the changing data at a specific point to database and pushes the same to the cache upon test execution for regression."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Availabilty Dashboard",
+                        "highlight": "Spring Boot, React and MongoDb",
+                        "summary": "<strong><em>Tata Consultancy Services, Kochi, Kerala, India</em></strong>",
+                        "descriptionType": null,
+                        "description": [
+                            "Environment availabilty service with React based <a href='https://abdus-shakur.github.io/api-health-dashboard-react'>UI Dashboard</a> which displays the availability of test environments."
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            },
+            {
+                "description": null,
+                "category": "UI Automation",
+                "align": "RIGHT",
+                "categoryDetails": [
+                    {
+                        "title": "Automation Framework",
+                        "highlight": "Framework Enhancement",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Designed an Automation framework based on selenium implementing a Page-Object Model, Extent Report and query based data extraction from excel with filtering algorithm to speed up scripting upto 5X for test suites."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Automation Tool",
+                        "highlight": "Value Addition",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Created an application specific tool which follow the specified pattern of xpath extraction. To completely automate the process of navigations thereby increasing automation teams productivity."
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            },
+            {
+                "description": null,
+                "category": "API Automation",
+                "align": "RIGHT",
+                "categoryDetails": [
+                    {
+                        "title": "Custom API Framework",
+                        "highlight": "Automation framework",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Created a REST-Assured based automation framework with in-built dynamic header management and embedded OAuth token generation mechanism with Apache poi based test data and integrated mongoDb support for centralized and parallel test suite execution."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Velocity template integration",
+                        "highlight": "Framework Enhancement",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Dynamic generation of json upon test execution to enable hassle free test data maintenance with increased productivity."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Jira Integration",
+                        "highlight": "Jira , TestNG",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Embedded Jira Integration to the custom developed framework. Which enables local or over the cloud executed test cases to reflect the live result status in jira eliminating the need of manual updation"
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            },
+            {
+                "description": null,
+                "category": "Devops",
+                "align": "RIGHT",
+                "categoryDetails": [
+                    {
+                        "title": "Single Touch Regression Suite",
+                        "highlight": "Jenkins",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Enabled test cases from multiple projects to be executed in multiple environments with a simple customizable parametrized jenkins job."
+                        ],
+                        "shortDescription": null
+                    },
+                    {
+                        "title": "Health Check Report",
+                        "highlight": "Jenkins",
+                        "summary": null,
+                        "descriptionType": null,
+                        "description": [
+                            "Highly flexible environment monitoring tool for over 20+ Api's in 12 enviroment with scheduled reports being sent to the clients."
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            }
+        ]
+    },
+    "resume": {
+        "description": "I'm an Experienced Automation Engineer with a demonstrable history of working in the information technology and services industry. Skilled in API Automation, JMeter, Maven, Agile Methodologies, Quality Assurance, and UI Automation. Strong engineering professional with a Bachelor of Engineering - B.E. focused in Automation Testing and Web Development and all related Technologies.",
+        "details": [
+            {
+                "description": null,
+                "category": "Summary",
+                "align": "LEFT",
+                "categoryDetails": [
+                    {
+                        "title": "Abdus Shakur",
+                        "highlight": null,
+                        "summary": "<br/><em>Highly dedicated Quality Assurance Automation Engineer & Developer, Offering with over <strong><em><span id='experience'>2.88</span></em></strong> years of Industry Experience in Automated Testing.</em>",
+                        "descriptionType": "bulletin",
+                        "description": [
+                            "<em>Madurai, TamilNadu, India.</em>",
+                            "<em>abdusshakurt@gmail.com</em>"
+                        ],
+                        "shortDescription": null
+                    }
+                ]
+            },
+            {
+                "description": null,
+                "category": "Professional Experience",
+                "align": "LEFT",
+                "categoryDetails": [
+                    {
+                        "title": "Quality Assurance Automation Lead",
+                        "highlight": "Apr 2020 - Present",
+                        "summary": "<strong><em>Tata Consultancy Services, Kochi, Kerala, India</em></strong>",
+                        "descriptionType": "bullet",
+                        "description": [
+                            "Consistently received highest band for three years along with 2X pay rise after clearing an organization level high talent capablity assessment in 2021",
+                            "Capable of managing multiple user stories simultaneously even of high complexities."
+                        ],
+                        "shortDescription": [
+                            "Presented live demo to clients for newly created custom framework from scratch with multiple features.",
+                            "Lead in the design, development and implementation of the automation tests",
+                            "Delegate tasks to the members of the team and provide counsel on all aspects of the project.",
+                            "Had a clear idea of how the app is supposed to work in every aspect. Set up a test environment in alignment with all the requirements",
+                            "Manage Offshore Testing Team and Ensure daily tasks to be promptly fulfilled & comments to be updated in Effort tracking tool.",
+                            "Fulfilled all the stories/tasks committed to at the start of the sprint/day, zero spillovers.Participate on Scrum stand up, Sprint grooming, Sprint planning & retrospection connects.",
+                            "DevOps: Integrate the automation project with Dev project & Dev pipeline thereby. With every deployment the regression suite to run smoothly. Also ensure JIRA integration of automation status is done by end of each sprint."
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+};
+export default profileData;
